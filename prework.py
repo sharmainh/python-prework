08189ae7f4ea8de2a629ce07d81e57e122427318
Sharmain <sharmainhenderson@Sharmains-MacBook-Air.local>
2023-01-23 19:51:40 -0800

    edit comments

--- prework.py
+++ prework.py
@@ -1,5 +1,6 @@
 #Question 1:
-#Write a function to print "hello_USERNAME!" USERNAME is the input of the                                          function. The first line of the code has been defined as below.
+#Write a function to print "hello_USERNAME!" USERNAME is the input of the                                          
+# function. The first line of the code has been defined as below.
 
 #function w/o return statement
 def hello_name(username):
@@ -8,7 +9,7 @@ def hello_name(username):
 
 hello_name("Sharmain")
 
-#function w/return statement
+#function w/return 
 
 def hello_name(username):
     """Print greeting upon receiving user input"""
@@ -19,7 +20,8 @@ print(hello_name("Shar"))
 
 
 #Question 2:
-#Write a python function, first_odds that prints the odd numbers from 1-100                                                and returns nothing.
+#Write a python function, first_odds that prints the odd numbers from 1-100                                                
+# and returns nothing.
 
 def first_odds():
     """Print odd numbers 1-100"""
@@ -31,11 +33,13 @@ first_odds()
 
 
 #Question 3:
-#Please write a Python function, max_num_in_list to return the max number of a                                          given list. The first line of the code has been defined as below.
+#Please write a Python function, max_num_in_list to return the max number of a                                          
+# given list. The first line of the code has been defined as below.
 
 def max_num_in_list(a_list):
     """Return the max number of a given list"""
-#The max() function returns the item with the highest value, or the item with                                             the highest value in an iterable
+#The max() function returns the item with the highest value, or the item with                                             
+# the highest value in an iterable
     maximum_num = max(a_list)
     return maximum_num
 
@@ -44,7 +48,9 @@ print(max_num_in_list(a_list))
 
 
 #Question 4:
-#Write a function to return if the given year is a leap year. A leap year is                                         divisible by 4, but not divisible by 100, unless it is also divisible by 400.                                            The return should be boolean Type (true/false).
+#Write a function to return if the given year is a leap year. A leap year is                                         
+# divisible by 4, but not divisible by 100, unless it is also divisible by 400.                                            
+# The return should be boolean Type (true/false).
 
 def is_leap_year(a_year):
     '''
@@ -57,15 +63,18 @@ def is_leap_year(a_year):
     else:
         return False
 print(is_leap_year(2015))
-# It was difficult trying to figure out which year is a leap year to write my                                     conditional statements. The following website defined a leap year in a way that                                            I could understand:                                                                                                  https://bjc.edc.org/Sept2015/bjc-r/cur/programming/3.5-T1PP/projects/checking-for-leap-years.html?topic=nyc_bjc%2F2-conditionals-abstraction.topic&course=bjc4nyc_2015-2016.html&novideo&noassignment#:~:text=(We%20aren't%20making%20this,(not%20divisible%20by%204).  
+# It was difficult trying to figure out which year is a leap year to write my                                     
+# conditional statements. The following website defined a leap year in a way 
+# that I could understand:                                                                                                  
+# https://bjc.edc.org/Sept2015/bjc-r/cur/programming/3.5-T1PP/projects/checking-for-leap-years.html?
+# topic=nyc_bjc%2F2-conditionals-abstraction.topic&course=bjc4nyc_2015-2016.html&novideo&noassignment#:~:text=
+# (We%20aren't%20making%20this,(not%20divisible%20by%204).  
 
 
 #Question 5:
-#Write a function to check to see if all numbers in list are consecutive numbers.                                        For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not                                         consecutive numbers. The return should be boolean Type.
-
-
-   
-# The sort function sorts the list so its in consecutive order. Once the list is                                       sorted THEN you can check to see if the original list is in order/matches the                                          sorted list
+#Write a function to check to see if all numbers in list are consecutive numbers.                                        
+#For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not                                         
+#consecutive numbers. The return should be boolean Type.
 
 def is_consecutive(a_list):
     """Return true if all numbers in list are consecutive otherwise false """
@@ -79,3 +88,4 @@ def is_consecutive(a_list):
     
 a_list = [2,3,4,5,6,7]
 print(is_consecutive(a_list)) 
+
